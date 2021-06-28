723a87733fea08b75dc969b384cc5c6b0df2a08b
Gustavo <gmcotta34@gmail.com>
2021-06-28 20:32:29 -0300

    feat: add LinkWrapper on Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import dynamic from 'next/dynamic'
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
+
+import LinkWrapper from 'components/LinkWrapper'
 
 const MapComponent = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <MapComponent />
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} aria-label="About" />
+      </LinkWrapper>
+      <MapComponent />
+    </>
+  )
 }
