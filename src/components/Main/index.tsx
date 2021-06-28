967ec3af24fcc23d63b2fef0f74ca3bbb430ce42
Gustavo <gmcotta34@gmail.com>
2021-06-27 22:18:10 -0300

    feat: add global variables and style

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,12 @@
-const Main = () => <h1>NextJS na prática</h1>
+import Link from 'next/link'
+
+const Main = () => (
+  <div>
+    <h1>NextJS na prática</h1>
+    <Link href="/">
+      <a>Link</a>
+    </Link>
+  </div>
+)
 
 export default Main
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,10 +13,34 @@ const GlobalStyles = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background-color: var(--background);
+    color: var(--white);
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p,
+  a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
+  }
+
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eee;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
   }
 `
 
