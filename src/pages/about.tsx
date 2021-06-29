e83a0eac023ec2cb4b640549696e0faabe59d4d8
Gustavo <gmcotta34@gmail.com>
2021-06-28 21:38:27 -0300

    feat: create getStaticPaths for About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,28 @@
 import client from 'graphql/client'
 import { GET_PAGES } from 'graphql/queries'
+import { useRouter } from 'next/dist/client/router'
 
 import PageTemplate from 'templates/Pages'
 
 export default function AboutPage() {
+  const router = useRouter()
+
+  if (router.isFallback) return <h1>Loading...</h1>
   return <PageTemplate />
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const paths = pages.map(({ slug }: never) => {
+    return {
+      params: slug
+    }
+  })
+
+  return {
+    paths,
+    fallback: true
+  }
 }
 
 export async function getStaticProps() {
