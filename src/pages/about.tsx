c6a1052c2a806b8eb7aa7fc2eb6e54c446f4d14e
Gustavo <gmcotta34@gmail.com>
2021-06-28 21:27:15 -0300

    feat: create graphql queries to get pages info

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,18 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
+
 import AboutTemplate from 'templates/About'
 
-export default function About() {
+export default function AboutPage() {
   return <AboutTemplate />
+}
+
+export async function getStaticProps() {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
 }
