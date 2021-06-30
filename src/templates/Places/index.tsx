03b536d3209cde91425652be9f5513c6799f549b
Gustavo <gmcotta34@gmail.com>
2021-06-30 19:17:33 -0300

    feat: style place template and use Image component from next

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image'
+
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+import { useRouter } from 'next/router'
+
 type ImageProps = {
   url: string
   height: number
@@ -8,21 +17,42 @@ export type PlacesTemplateProps = {
   place: {
     slug: string
     name: string
-    description: {
-      html: string
+    description?: {
+      html: string | undefined
     }
     gallery: ImageProps[]
   }
 }
 
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  const router = useRouter()
+  if (router.isFallback) return <h1>Carregando...</h1>
+
   return (
     <>
-      <h1>{place.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
-      {place.gallery.map((photo, index) => (
-        <img key={`photo-${index}`} src={photo.url} alt={place.name} />
-      ))}
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
+          />
+          <S.Gallery>
+            {place.gallery.map((photo, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={photo.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   )
 }
--- src/templates/Places/styles.ts
+++ src/templates/Places/styles.ts
@@ -1,3 +1,50 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  padding: var(--large) var(--medium);
+`
+
+export const Container = styled.section`
+  max-width: var(--container);
+  margin: auto;
+`
+
+export const Heading = styled.h1`
+  font-size: var(--large);
+  margin-bottom: var(--medium);
+`
+
+export const Body = styled.div`
+  margin-bottom: var(--large);
+
+  p {
+    margin-bottom: var(--medium);
+  }
+`
+
+export const Gallery = styled.div`
+  display: grid;
+  grid-gap: var(--medium);
+
+  img {
+    background-color: #363636;
+    background-image: linear-gradient(
+      to right,
+      #363636 0%,
+      #1f1f1f 20%,
+      #363636 40%,
+      #363636 100%
+    );
+    background-size: 80rem 14rem;
+    animation: placeholderShimmer 1s linear infinite forwards;
+
+    @keyframes placeholderShimmer {
+      0% {
+        background-position: -40rem 0;
+      }
+      100% {
+        background-position: 40rem 0;
+      }
+    }
+  }
+`
